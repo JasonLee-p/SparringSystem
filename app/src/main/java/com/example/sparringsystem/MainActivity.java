5073f026ac34a5b61188a0ccbcfbc3fe87873154
JasonLee-p <2593292614@qq.com>
2024-05-16 17:54:08 +0800

    四大fragment

--- app/src/main/java/com/example/sparringsystem/MainActivity.java
+++ app/src/main/java/com/example/sparringsystem/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.sparringsystem;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.view.MenuItem;
@@ -19,20 +21,37 @@ public class MainActivity extends AppCompatActivity {
         // 加载底部导航栏
         BottomNavigationView navView = findViewById(R.id.nav_view);
         navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        // 默认加载HomeFragment
+        loadFragment(new HomeFragment());
     }
+
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener =
             item -> {
-                int _id = item.getItemId();
-                if (_id == R.id.navigation_home) {
+                int active_item = item.getItemId();
+                if (active_item == R.id.navigation_home) {
                     // 主页
+                    loadFragment(new HomeFragment());
                     return true;
-                } else if (_id == R.id.navigation_music) {
-                    // 音乐
+                } else if (active_item == R.id.navigation_tuning) {
+                    // 调音界面
+                    loadFragment(new TuningFragment());
                     return true;
-                } else if (_id == R.id.navigation_notifications) {
+                } else if (active_item == R.id.navigation_music) {
+                    // 练习界面
+                    loadFragment(new PracticeFragment());
+                    return true;
+                } else if (active_item == R.id.navigation_notifications) {
                     // 通知
+                    loadFragment(new NavigationFragment());
                     return true;
                 }
                 return false;
             };
+
+    private void loadFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame_container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
 }
