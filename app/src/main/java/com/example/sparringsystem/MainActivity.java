4d8bbb94ded5628aaa71e7a1b709b091c95fe5bb
JasonLee-p <2593292614@qq.com>
2024-05-16 10:54:38 +0800

    底部栏

--- app/src/main/java/com/example/sparringsystem/MainActivity.java
+++ app/src/main/java/com/example/sparringsystem/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.sparringsystem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // 加载底部导航栏
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener =
+            item -> {
+                int _id = item.getItemId();
+                if (_id == R.id.navigation_home) {
+                    // 主页
+                    return true;
+                } else if (_id == R.id.navigation_music) {
+                    // 音乐
+                    return true;
+                } else if (_id == R.id.navigation_notifications) {
+                    // 通知
+                    return true;
+                }
+                return false;
+            };
 }
